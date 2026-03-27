bf218eb08901ea8abfc3bd1630df04e0c9f90e8f
K3Jonass <ycouraj@gmail.com>
2026-03-27 12:18:37 +0100

    add vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://profithub.cloud"),
@@ -48,7 +50,16 @@ export default function RootLayout({
         <Navbar />
         <main>{children}</main>
         <Footer />
+      {/* 🔥 Vercel Analytics */}
+        <Analytics />
+
+        {/* 🚀 Speed Insights */}
+        <SpeedInsights />
       </body>
     </html>
-  );
+  )
 }
+
+
+
+        
